95667562c6eaceaa46d1d42cd0958c3d93847346
beart-ccc <114675396+beart-ccc@users.noreply.github.com>
2022-10-14 14:24:07 +0100

    Update Create Surpass Shortcut.ps1

--- Create Surpass Shortcut.ps1
+++ Create Surpass Shortcut.ps1
@@ -3,5 +3,5 @@
    $WScriptShell = New-Object -ComObject WScript.Shell
    $Shortcut = $WScriptShell.CreateShortcut($strLinkFile)
    $Shortcut.TargetPath = $strTargetPath
-   $Shortcut.IconLocation = "%USERPROFILE%\AppData\Local\SurpassViewer\Surpass Viewer.exe,mun,0"
+   $Shortcut.IconLocation = "%USERPROFILE%\AppData\Local\SurpassViewer\Surpass Viewer.exe.mun, 0"
    $Shortcut.Save()
